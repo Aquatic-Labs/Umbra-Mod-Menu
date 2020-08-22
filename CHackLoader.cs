b37da32a461b4a3cf7daf1ec10f89a5e2f65634b
Acher0ns <kamron.cole13@gmail.com>
2020-08-22 02:11:16 -0400

    Improve how the menu is loaded
    Credit to Shalzuth

--- CHackLoader.cs
+++ CHackLoader.cs
@@ -1,16 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 namespace UmbraRoR
 {
     public class Loader
     {
-        static GameObject gameObject;
+        //static GameObject gameObject;
 
-        public static void Load()
+        /*public static void Load()
         {
             gameObject = new GameObject();
             gameObject.AddComponent<Main>();
             Object.DontDestroyOnLoad(gameObject);
+            Utility.LoadAssembly();
+            Updates.CheckForUpdate();
+        }*/
+
+        public static GameObject gameObject;
+
+        public static void Load()
+        {
+            //RoR2.RoR2Application.isModded = true;
+            while (gameObject = GameObject.Find("Umbra Menu"))
+                GameObject.Destroy(gameObject);
+            gameObject = new GameObject("Umbra Menu");
+            Object.DontDestroyOnLoad(gameObject);
+            gameObject.SetActive(false);
+            var types = Assembly.GetExecutingAssembly().GetTypes().ToList().Where(t => t.BaseType == typeof(MonoBehaviour) && !t.IsNested);
+            foreach (var type in types)
+            {
+                var component = (MonoBehaviour)gameObject.AddComponent(type);
+                component.enabled = false;
+            }
+            gameObject.GetComponent<Main>().enabled = true;
+            gameObject.SetActive(true);
             Utility.LoadAssembly();
             Updates.CheckForUpdate();
         }
